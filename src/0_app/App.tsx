860ef93801a8a74b1086aad108fedd917aca5ed9
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2026-02-17 16:13:15 +0300

    Start new project

---
+++ src/0_app/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@shared/ui/shadcn/button";
+
+function App() {
+    return (
+        <div>
+            <Button>Rick and Morty API</Button>
+        </div>
+    );
+}
+
+export default App;
